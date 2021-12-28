ae0eed59513df341d5a329129694cd2d94de4e30
venlancer <venlancer@gmail.com>
2021-12-28 18:15:17 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'supporttask';
+export class AppComponent implements OnInit {
+  comments: string;
+  count: number;
+  constructor() { }
+
+  ngOnInit() {
+    this.count = 0;
+  }
+
+  receiveComment($event) {
+    this.comments = $event;
+    this.count = this.comments.length;
+    console.log(this.comments.length);
+  }
+
+  recieveCount($event) {
+    this.comments = $event;
+    this.count = this.comments.length;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
-import { AdminComponent } from './admin/admin.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CommentboxComponent } from './commentbox/commentbox.component';
+import { CommentsComponent } from './comments/comments.component';
+import { ChildboxComponent } from './childbox/childbox.component';
+import { DatacontainerDirective } from './comments/comments.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    AdminComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    CommentboxComponent,
+    CommentsComponent,
+    ChildboxComponent,
+    DatacontainerDirective
   ],
   providers: [],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    CommonModule
+  ],
+  entryComponents: [ChildboxComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
